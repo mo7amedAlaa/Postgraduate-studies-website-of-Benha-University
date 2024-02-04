8f8f3c812e9209277b7979e9f75a31820ac38de2
mo7medAlaa <mohamedalhakar5@gmail.com>
2024-02-04 23:04:16 +0200

    fddfdf

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -2,6 +2,4 @@ import personaLogo from './images/persona-logo.jpg';
 import graduatedMenIcon from './images/postgraduate.png';
 import facLogo from './images/شعار الكلية.png';
 import uniLogo from './images/شعار_جامعة_بنها.png';
-import Laypdf from './pdf/new_lay_premaster_2021.pdf';
-import bg1 from './images/bg-gradute1.png';
-export { personaLogo, graduatedMenIcon, facLogo, uniLogo, Laypdf, bg1 };
+export { personaLogo, graduatedMenIcon, facLogo, uniLogo };
--- src/pages/usr-adminPages/Page1.jsx
+++ src/pages/usr-adminPages/Page1.jsx
@@ -1,16 +1,5 @@
-import { Laypdf } from '../../assets';
 import MainLayout from '../../component/Main/MainLayout';
 
 export default function Page1() {
-  return (
-    <MainLayout>
-      <embed
-        src={Laypdf}
-        className="rounded-md"
-        type="application/pdf"
-        width="100%"
-        height="100%"
-      />
-    </MainLayout>
-  );
+  return <MainLayout></MainLayout>;
 }
