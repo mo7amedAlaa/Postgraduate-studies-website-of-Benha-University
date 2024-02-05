54f8b48ec9b9d19eb34381176dd930d36026a950
Mohamed Al-Sayed <111025962+MohamedElsayed2022@users.noreply.github.com>
2024-02-05 18:52:10 +0200

    Last Editing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,11 @@ import { MdOutlinePhoneEnabled } from "react-icons/md";
 import { uniLogo } from "../assets";
 import { Link } from "react-router-dom";
 import ImageProgram from "../assets/images/program.jpg";
+import { FiPhone } from "react-icons/fi";
+import { CiLocationOn } from "react-icons/ci";
+import { HiOutlineMail } from "react-icons/hi";
+import Footer from "../component/Footer/Footer";
+
 function Home() {
   return (
     <>
@@ -69,7 +74,7 @@ function Home() {
           <div className="flex items-end">
             <Link
               to={"/login"}
-              className=" bg-gradient-to-t w-[13rem] bg-main hover:bg-[#5dc6f0] p-2 rounded-md  text-2xl mt-9 text-white font-serif font-semibold cursor-pointer transition-[4rem] "
+              className=" bg-gradient-to-t w-[13rem] from-[#51ACD0] to-[#5dc6f0] p-2 rounded-md  text-2xl mt-9 text-white font-serif font-semibold cursor-pointer hover:bg-teal-400  "
             >
               تسجيل الدخول
             </Link>
@@ -82,8 +87,8 @@ function Home() {
         </h1>
       </div>
       <div className="services flex justify-center items-center gap-[3rem] flex-wrap  p-3 font-sans  mt-20">
-        <div className="w-[70%] grid grid-cols-4  gap-3 ">
-          <div className="shadow-lg text-center mb-10 rounded-lg w-60 p-5 hover:scale-110 transition-all">
+        <div className="w-[80%] grid grid-cols-4  gap-3 ">
+          <div className="shadow-lg text-center mb-10 rounded-lg w-70 p-5 hover:scale-110 transition-all">
             <div className="flex items-center justify-center">
               <img
                 src="https://www.asu.edu.eg/141090/_mediacenter/2020/png/372c6de72afbddfc800432ce0f8b178b.png"
@@ -96,7 +101,7 @@ function Home() {
               التقدم للدراسات العليا
             </h3>
           </div>
-          <div className="shadow-lg text-center mb-10 rounded-lg w-60 p-5 hover:scale-110 transition-all">
+          <div className="shadow-lg text-center mb-10 rounded-lg w-70 p-5 hover:scale-110 transition-all">
             <div className="flex items-center justify-center">
               <img
                 src="https://www.asu.edu.eg/141090/_mediacenter/2021/png/6cb561a4bbfa874570ca82c711886eeb.png"
@@ -110,7 +115,7 @@ function Home() {
               تسجيل واختيار المواد
             </h3>
           </div>
-          <div className="shadow-lg text-center mb-10 rounded-lg w-60 p-5 hover:scale-110 transition-all">
+          <div className="shadow-lg text-center mb-10 rounded-lg w-70 p-5 hover:scale-110 transition-all">
             <div className="flex items-center justify-center">
               <img
                 src="https://www.asu.edu.eg/141090/_mediacenter/2020/png/ec5aecb3aadf2077d1f2756ede50ee97.png"
@@ -124,7 +129,7 @@ function Home() {
               تسجيل واختيار المواد
             </h3>
           </div>
-          <div className="shadow-lg text-center mb-10 rounded-lg w-60 p-5 hover:scale-110 transition-all">
+          <div className="shadow-lg text-center mb-10 rounded-lg w-70 p-5 hover:scale-110 transition-all">
             <div className="flex items-center justify-center">
               <img
                 src="https://www.asu.edu.eg/141090/_mediacenter/2020/png/8f8b9a7cc9c5d1c4df4e229124bfe7e0.png"
@@ -138,7 +143,7 @@ function Home() {
               نتائج الدراسات العليا{" "}
             </h3>
           </div>
-          <div className="shadow-lg text-center mb-10 rounded-lg w-60 p-5 hover:scale-110 transition-all">
+          <div className="shadow-lg text-center mb-10 rounded-lg w-70 p-5 hover:scale-110 transition-all">
             <div className="flex items-center justify-center">
               <img
                 src="https://www.asu.edu.eg/141090/_mediacenter/2022/jpg/becd7d7bdb32c94bde115a7e10927cec.jpg"
@@ -149,7 +154,7 @@ function Home() {
 
             <h3 className="mt-5 mb-5 font-Poppins text-xl"> كورسات كورسيرا </h3>
           </div>
-          <div className="shadow-lg text-center mb-10 rounded-lg w-60 p-5 hover:scale-110 transition-all">
+          <div className="shadow-lg text-center mb-10 rounded-lg w-70 p-5 hover:scale-110 transition-all">
             <div className="flex items-center justify-center">
               <img
                 src="https://www.asu.edu.eg/141090/_mediacenter/2021/png/4aa70b2c4a21785c4b872fe5e6be37d2.png"
@@ -162,10 +167,10 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="graduates mt-5 mb-5">
-        <div className="ms-10 ">
+      <div className="graduates mt-5 mb-5 flex justify-center flex-col">
+        <div className="ms-10  w-[80%] gap-10 text-center">
           <h2 className="bg-main text-white text-4xl p-2 font-sans w-[80%] text-center rounded-md">
-            دراسات العليا
+            الدراسات العليا
           </h2>
         </div>
         <div className="ms-10">
@@ -208,7 +213,7 @@ function Home() {
         </h1>
       </div>
       <div className="programs flex justify-center items-center gap-[3rem] flex-wrap  p-3 font-sans  mt-20">
-        <div className="w-[80%] grid grid-cols-3  gap-10 ">
+        <div className="w-[80%] grid grid-cols-3  gap-3 ">
           <div className="card-program  mb-10 border shadow-md rounded-md hover:scale-110 transition-all ">
             <img
               src="https://www.asu.edu.eg/images/3faf5cd9-e1a6-443a-ab3f-9be87e867a28.jpg"
@@ -249,7 +254,7 @@ function Home() {
               الجامعة، واستمارات التقدم والمستندات المطلوبة للالتحاق، إضافة إلى
               المصروفات المطلوبة من المصريين والوافدين، إلى جانب الدورات
               التدريبية المتاحة لطلاب الدراسات العليا.. وفيما يلي شرحًا توضيحًا
-              يتضمن كل هذه الجوانب وأكثر..
+              يتضمن كل هذه الجوانب وأكثر.
             </p>
             <div className="flex items-end">
               <button className="text-white bg-main hover:bg-[#5dc6f0]  w-[13rem]  text-2xl mt-9 p-2 font-serif font-semibold cursor-pointer rounded-md transition-[2rem]">
@@ -266,10 +271,9 @@ function Home() {
           </div>
         </div>
       </div>
-
-      <div className="tot_num mb-20 bg-black mt-15 h-96 flex justify-center items-center gap-[3rem] flex-wrap  p-3 font-sans  mt-20 ">
+      <div className="tot_num bg-black mt-15 h-96 flex justify-center items-center gap-[3rem] flex-wrap  p-3 font-sans mb-20 mt-20  ">
         <div className="w-[80%] grid grid-cols-4 gap-4 text-center">
-          <div className="border rounded-sm text-white h-[200px] hh text-center hover:border-main transition-all p-3">
+          <div className="border rounded-sm text-white h-[180px] hh text-center hover:border-main transition-all p-2">
             <div className="">
               <p className="font-sans text-5xl font-medium text-white text-center mt-5">
                 54
@@ -279,7 +283,7 @@ function Home() {
               </h1>
             </div>
           </div>
-          <div className="border rounded-sm text-white h-[200px] hh text-center hover:border-main transition-all p-3">
+          <div className="border rounded-sm text-white h-[180px] hh text-center hover:border-main transition-all p-2">
             <div className="">
               <p className="font-sans text-5xl font-medium text-white text-center mt-5">
                 55
@@ -290,7 +294,7 @@ function Home() {
               </h1>
             </div>
           </div>
-          <div className="border rounded-sm text-white h-[200px] hh text-center hover:border-main transition-all p-3">
+          <div className="border rounded-sm text-white h-[180px] hh text-center hover:border-main transition-all">
             <div className="">
               <p className="font-sans text-5xl font-medium text-white text-center mt-5">
                 78
@@ -301,7 +305,7 @@ function Home() {
               </h1>
             </div>
           </div>
-          <div className="border rounded-sm text-white h-[200px] hh text-center hover:border-main transition-all p-3">
+          <div className="border rounded-sm text-white h-[180px] hh text-center hover:border-main transition-all">
             <div className="">
               <p className="font-sans text-5xl font-medium text-white text-center mt-5">
                 99
@@ -314,15 +318,14 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="sec_search  flex justify-center mb-10  bg-">
-        <div className="grid grid-cols-2  w-[80%] gap-5 text-center p-3">
-          <div className="overlay_content p-5">
+      <div className="descri flex justify-center mb-10 mt-20 b">
+        <div className="grid grid-cols-2  w-[80%] gap-10 text-center">
+          <div className="overlay_content">
             <h1 className="text-start font-sans text-5xl font-semibold">
-              {" "}
-              البحث العلمى{" "}
+              البحث العلمى
             </h1>
             <p className="text-start font-sans text-2xl mt-5">
-              يعد الباحثين من بين الفئات المهمة في جامعة بنها.. لذا يحرص قطاع
+              يعد الباحثين من بين الفئات المهمة في جامعة عين شمس.. لذا يحرص قطاع
               الدراسات العليا والبحوث على توفير كافة السبل وأفضلها لتقديم
               الخدمات العلمية والبحثية والتعليمة التي تساهم في مساعدتهم على
               الرقي بالبحث العلمي وتكون في الوقت نفسه عاملا محفزا لهم على
@@ -336,7 +339,7 @@ function Home() {
               </button>
             </div>
           </div>
-          <div className="imagee  text-center mt-[50px] sm:mt-[3rem]">
+          <div className="imagee  text-center">
             <img
               src="https://www.asu.edu.eg/images/16.jpg"
               className="w-[100%] rounded-lg"
@@ -345,6 +348,52 @@ function Home() {
           </div>
         </div>
       </div>
+      <div className="address_details flex justify-center mt-20 shadow-2xl">
+        <div className="content grid grid-cols-3  w-[80%] gap-10 text-center bg-black  rounded-3xl p-5 ">
+          <div className="flex flex-col items-center">
+            <CiLocationOn
+              className="color-white flex justify-center items-center  "
+              size={30}
+              color="white"
+            />
+            <h1 className="font-sans text-2xl font-semibold text-white mt-3">
+              العنوان
+            </h1>
+            <p className="p color-white font-sans text-xl mt-5">
+              طريق اسكندرية الزراعى
+            </p>
+          </div>
+          <div className="flex flex-col items-center">
+            <FiPhone
+              className="color-white flex justify-center items-center  "
+              size={30}
+              color="white"
+            />
+            <h1 className="font-sans text-2xl font-semibold text-white mt-3">
+              رقم التليفون
+            </h1>
+            <p className="p color-white font-sans text-xl mt-5">
+              {" "}
+              +20 106 528 4676
+            </p>
+          </div>
+          <div className="flex flex-col items-center">
+            <HiOutlineMail
+              className="color-white flex justify-center items-center  "
+              size={30}
+              color="white"
+            />
+            <h1 className="font-sans text-2xl font-semibold text-white mt-3">
+              البريد الالكترونى
+            </h1>
+            <p className="p color-white font-sans text-xl mt-5">
+              {" "}
+              mohamedalsayedabdullah@gmail.com
+            </p>
+          </div>{" "}
+        </div>
+      </div>
+      <Footer />
     </>
   );
 }
