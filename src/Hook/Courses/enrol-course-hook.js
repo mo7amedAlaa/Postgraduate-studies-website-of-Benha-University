6408e8b91068bdd51e2b0a6ec1e8008952ed866d
Mohamed Al-Sayed <111025962+MohamedElsayed2022@users.noreply.github.com>
2024-05-10 20:32:56 +0300

    Enroll Course With Enroll Course Hook Cheak it

---
+++ src/Hook/Courses/enrol-course-hook.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCourses } from "../../Redux/Actions/coursesAction";
+
+const EnrolCourseHook = () => {
+    const dispatch = useDispatch()
+    useEffect(()=>{
+      dispatch(getAllCourses())
+    },[])
+    const [totalcourses, setTotalCourses] = useState([]);
+    const [loading, setLoading] = useState(true);
+  
+  const allcourses = useSelector((state) => state.allCourses.courses);
+  
+  useEffect(() => {
+    if (allcourses) {
+      setTotalCourses(allcourses);
+      setLoading(false)
+      console.log(loading)
+      console.log(totalcourses);
+    } else {
+      setTotalCourses([]);
+      setLoading(true)
+    }
+  }, [allcourses]);
+  
+    const columns = [
+      { field: 'id', headerName: 'ID', width: 70 },
+      { field: 'code', headerName: 'كود المادة', width: 130 },
+      { field: 'subName', headerName: 'اسم المادة', width: 130 },
+      { field: 'hourNum', headerName: 'عدد الساعات', width: 130 },
+      { field: 'maxGrade', headerName: 'الدرجة العظمي', width: 130 },
+      { field: 'minGrade', headerName: 'الدرجة الصغري', width: 130 },
+    ];
+
+    return [totalcourses ,columns ]
+ 
+}
+
+export default EnrolCourseHook
