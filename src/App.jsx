0af6ba49e69e590da0d3ea38dc61f95a575b3493
mo7medAlaa <mohamedalhakar5@gmail.com>
2024-06-19 13:39:35 +0300

    gh

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import React, { useContext } from 'react';
 import {
   Navigate,
   RouterProvider,
   createBrowserRouter,
 } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import './App.css';
 import './pages/chat/chatStyle.css';
@@ -37,28 +41,45 @@ import StudyTable from './pages/User/StudyTable';
 import HomeChat from './pages/chat/HomeChat';
 import LoginChat from './pages/chat/LoginChat';
 import RegisterChat from './pages/chat/RegisterChat';
-import { useContext } from 'react';
 import { AuthContext } from './context/AuthContext';
 import RestPassword from './pages/MainPages/restPassword';
 import { useTranslation } from 'react-i18next';
 import AcceptStudent from './pages/Head of Department/AcceptStudent';
 import UserProfile from './component/AccountSetting/UserProfile';
-import { useSelector } from 'react-redux';
+import Swal from 'sweetalert2';
 
 function App() {
   const { i18n } = useTranslation();
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
   };
-  const { currentUser } = useContext(AuthContext);
-  const userInfo = useSelector((state) => state.user.UserInfo);
-  const ProtectedRoute = ({ children }) => {
+
+  const ProtectedChatRoute = ({ children }) => {
+    const { currentUser } = useContext(AuthContext);
     if (!currentUser) {
+      toast.error('You must be logged in to access the chat');
       return <Navigate to="/chatslogin" />;
     }
     return children;
   };
 
+  const ProtectedRoute = ({ children }) => {
+    const userInfo = useSelector((state) => state.user.UserInfo);
+    if (!userInfo) {
+      Swal.fire('Error', 'You must be logged in to access this page', 'error');
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
+
+  const PaidRoute = ({ children }) => {
+    const userPaid = useSelector((state) => state.user.UserInfo?.Payment);
+    if (userPaid === 'pending') {
+      toast.error('You must complete the payment first');
+      return <Navigate to="/payment" />;
+    }
+    return children;
+  };
   const router = createBrowserRouter([
     {
       path: '/',
@@ -66,10 +87,13 @@ function App() {
     },
     {
       path: '/employee',
-      element: <Layout Actions={employeeActions} />,
+      element: (
+        <ProtectedRoute>
+          <Layout Actions={employeeActions} />
+        </ProtectedRoute>
+      ),
       children: [
         { path: '/employee/Regulations', element: <Regulations /> },
-
         {
           path: '/employee/studentinformation',
           element: <Studentinformation />,
@@ -85,7 +109,11 @@ function App() {
     },
     {
       path: '/professor',
-      element: <Layout Actions={profActions} />,
+      element: (
+        <ProtectedRoute>
+          <Layout Actions={profActions} />
+        </ProtectedRoute>
+      ),
       children: [
         { path: '/professor/gradesstudent', element: <GradesStudent /> },
         { path: '/professor/uploadmaterial', element: <UploadMaterial /> },
@@ -93,20 +121,18 @@ function App() {
           path: '/professor/extendRestriction',
           element: <ExtendRestriction />,
         },
-        {
-          path: '/professor/studentstuts',
-          element: <StudentStuts />,
-        },
+        { path: '/professor/studentstuts', element: <StudentStuts /> },
       ],
     },
     {
       path: '/headofdepartment',
-      element: <Layout Actions={headActions} />,
+      element: (
+        <ProtectedRoute>
+          <Layout Actions={headActions} />
+        </ProtectedRoute>
+      ),
       children: [
-        {
-          path: '/headofdepartment/acceptstudent',
-          element: <AcceptStudent />,
-        },
+        { path: '/headofdepartment/acceptstudent', element: <AcceptStudent /> },
         {
           path: '/headofdepartment/applyplanresearch',
           element: <ApplyPlanResearch />,
@@ -119,49 +145,128 @@ function App() {
     },
     {
       path: '/admin',
-      element: <Layout Actions={AdminActions} />,
+      element: (
+        <ProtectedRoute>
+          <Layout Actions={AdminActions} />
+        </ProtectedRoute>
+      ),
       children: [
-        {
-          path: '/admin/GenerateAccount',
-          element: <GenerateAccounts />,
-        },
+        { path: '/admin/GenerateAccount', element: <GenerateAccounts /> },
       ],
     },
     {
       path: '/CollegeVice',
-      element: <Layout Actions={AdminActions} />,
-    },
-    { path: '/login', element: <Login /> },
-    { path: '/accountsetting', element: <UserProfile /> },
-    { path: '/restpassword', element: <RestPassword /> },
-    { path: '/payment', element: <Payment /> },
-    { path: '/registration', element: <RegistrationToApply /> },
-    { path: '/course', element: <EnrolCourse /> },
-    { path: '/material', element: <Material /> },
-    { path: '/showgrade', element: <Grade /> },
-    { path: '/recordpoint', element: <RecordPoint /> },
-    { path: '/examtable', element: <ExamTable /> },
-    { path: '/studytable', element: <StudyTable /> },
-    {
-      path: '/chats',
       element: (
         <ProtectedRoute>
-          <HomeChat />,
+          <Layout Actions={AdminActions} />
+        </ProtectedRoute>
+      ),
+    },
+    { path: '/login', element: <Login /> },
+    {
+      path: '/accountsetting',
+      element: (
+        <ProtectedRoute>
+          <UserProfile />
         </ProtectedRoute>
       ),
     },
     {
-      path: '/chatslogin',
-      element: <LoginChat />,
+      path: '/restpassword',
+      element: (
+        <ProtectedRoute>
+          <RestPassword />
+        </ProtectedRoute>
+      ),
     },
     {
-      path: '/chatsregister',
-      element: <RegisterChat />,
+      path: '/payment',
+      element: (
+        <ProtectedRoute>
+          <Payment />
+        </ProtectedRoute>
+      ),
     },
+    {
+      path: '/registration',
+      element: <RegistrationToApply />,
+    },
+    {
+      path: '/course',
+      element: (
+        <ProtectedRoute>
+          <PaidRoute>
+            <EnrolCourse />
+          </PaidRoute>
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: '/material',
+      element: (
+        <ProtectedRoute>
+          <PaidRoute>
+            <Material />
+          </PaidRoute>
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: '/showgrade',
+      element: (
+        <ProtectedRoute>
+          <PaidRoute>
+            <Grade />
+          </PaidRoute>
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: '/recordpoint',
+      element: (
+        <ProtectedRoute>
+          <PaidRoute>
+            <RecordPoint />
+          </PaidRoute>
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: '/examtable',
+      element: (
+        <ProtectedRoute>
+          <PaidRoute>
+            <ExamTable />
+          </PaidRoute>
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: '/studytable',
+      element: (
+        <ProtectedRoute>
+          <PaidRoute>
+            <StudyTable />
+          </PaidRoute>
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: '/chats',
+      element: (
+        <ProtectedChatRoute>
+          <HomeChat />
+        </ProtectedChatRoute>
+      ),
+    },
+    { path: '/chatslogin', element: <LoginChat /> },
+    { path: '/chatsregister', element: <RegisterChat /> },
   ]);
+
   return (
-    <div className="font-bodyFont min-h-screen  ">
+    <div className="font-bodyFont min-h-screen">
       <RouterProvider router={router} />
+      <ToastContainer />
     </div>
   );
 }
